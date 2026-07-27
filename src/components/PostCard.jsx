0f94b42625f65fc0738be53e82d100040a27272a
vivekchandravanshi769-ops <vivekchandravanshi769@gmail.com>
2026-07-27 19:43:37 +0530

    Build full-stack blog app with React, Redux and Appwrite

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import appwriteService from "../appwrite/config"
+import {Link} from 'react-router-dom'
+
+function PostCard({$id, title, featuredImage}) {
+    const imageUrl = featuredImage 
+        ? appwriteService.getFilePreview(featuredImage).toString()
+        : null
+    
+  return (
+    <Link to={`/post/${$id}`}>
+        <div className='w-full bg-gray-100 rounded-xl p-4'>
+            <div className='w-full justify-center mb-4'>
+                {imageUrl && <img src={imageUrl} alt={title}
+                className='rounded-xl' />}
+
+            </div>
+            <h2
+            className='text-xl font-bold'
+            >{title}</h2>
+        </div>
+    </Link>
+  )
+}
+
+
+export default PostCard
